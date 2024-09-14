b907fde3ae1b585807ea23e53a9e65246f766cdd
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 11:28:31 +0530

    started

---
+++ src/test/java/org/phone/SampleTest.java
@@ -0,0 +1,17 @@
+package org.phone;
+
+public class SampleTest {
+	private void name() {
+		System.out.println(43);
+		
+	}
+	private void name2() {
+		System.out.println(56);
+		
+		
+	}
+	private void name3() {
+		System.out.println(57);
+}
+
+}
